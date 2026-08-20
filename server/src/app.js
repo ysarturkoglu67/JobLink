5d98fe4f93ddec25b5597343fd75ccb8844a3ac5
Yasar Turkoglu <turkogluyasar@hotmail.com>
2026-08-20 23:38:56 +0300

    fix: production socket cors

--- server/src/app.js
+++ server/src/app.js
@@ -20,7 +20,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://kariyerinsa.onrender.com",
+    ],
     credentials: true,
   })
 );
--- server/src/server.js
+++ server/src/server.js
@@ -13,12 +13,14 @@ const server = http.createServer(app);
 
 export const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://kariyerinsa.onrender.com",
+    ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   },
 });
-
 const onlineUsers = new Map();
 
 io.on("connection", (socket) => {
